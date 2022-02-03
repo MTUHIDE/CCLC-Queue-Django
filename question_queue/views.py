aa87427d9f6e68d042ddd3da5a5ab858e3de74e0
tjzetty <tjzetty@mtu.edu>
2022-02-03 16:42:31 -0500

    Linked HTML and CSS files for each view and removed '_view' suffix from each view.

--- question_queue/views.py
+++ question_queue/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
 
 # Create your views here.
+def index(response):
+    template = loader.get_template('question_queue/index.html')
+    context = {'question_queue': ''}
+    return HttpResponse(template.render(context, response))
 
 
-def student_view(respnse):
-    return HttpResponse("You are at the student page!")
+def student(response):
+    template = loader.get_template('question_queue/student.html')
+    context = {'question_queue': ''}
+    return HttpResponse(template.render(context, response))
 
 
-def instructor_view(respnse):
-    return HttpResponse("You are at the instructor page!")
+def instructor(response):
+    template = loader.get_template('question_queue/instructor.html')
+    context = {'question_queue': ''}
+    return HttpResponse(template.render(context, response))
