6ddadcdeac0437da82d35eee932180b65669e805
TRMoosee <therealmoosee@gmail.com>
2022-03-04 11:15:49 -0500

    Updated Question model to match new ER Diagram

--- question_queue/models.py
+++ question_queue/models.py
@@ -2,22 +2,10 @@ from django.db import models
 
 
 class Question(models.Model):
-    version = models.BigIntegerField()
-    course_name = models.CharField(max_length=256)
-    is_deleted = models.BooleanField()
-    date_created = models.DateTimeField()
-    assignment_date = models.CharField(max_length=256)
-    last_updated = models.DateTimeField()
-    deleted_by_id = (
-        models.BigIntegerField()
-    )  # MAKE FOREIGN KEY WHEN USER TABLE IS ADDED
-    language = models.CharField(max_length=256)
-    message = models.TextField()
-    is_answered = models.BooleanField()
-    person_id = models.BigIntegerField()  # MAKE FOREIGN KEY WHEN USER TABLE IS ADDED
-    ipaddress = models.CharField(max_length=256)
-    hostname = models.CharField(max_length=256)
-    course = models.CharField(max_length=256)
-    is_hand_raised = models.BooleanField()
-    assignment = models.CharField(max_length=256)
-    subject = models.CharField(max_length=256)
+    id = models.BigIntegerField() # PRIMARY KEY
+    user_name = models.CharField(max_length=32) # Should be max 6 chars, but lets play it safe
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.EmailField()
+    password = models.CharField(max_length=256)
+    groups = models.CharField(max_length=256)
