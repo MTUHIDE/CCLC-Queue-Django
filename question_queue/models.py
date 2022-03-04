e322b5637fbbc0284a365f669f68443b4d8e51bd
kodew <kodewhitworth01@gmail.com>
2022-03-04 17:02:43 -0500

    Added Answer model and fixed a bug in the Question model regarding the PK

--- question_queue/models.py
+++ question_queue/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 
 class Question(models.Model):
-    id = models.BigIntegerField() # PRIMARY KEY
-    user_name = models.CharField(max_length=32) # Should be max 6 chars, but lets play it safe
+    user_name = models.CharField(
+        max_length=32
+    )  # Should be max 6 chars, but lets play it safe
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     email = models.EmailField()
     password = models.CharField(max_length=256)
     groups = models.CharField(max_length=256)
+
+
+class Answer(models.Model):
+    question_id = models.ForeignKey(
+        Question, on_delete=models.CASCADE
+    )  # Might need to change this
+    message = models.CharField(max_length=256)
+    created_at = models.DateTimeField()
+    last_updated = models.DateTimeField()
