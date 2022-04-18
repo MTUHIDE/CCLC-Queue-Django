ff3df6cd2e345590802dceaac4412839e46c37a3
Kode <kodewhitworth01@gmail.com>
2022-04-18 19:58:54 -0400

    Implemented the modal components

--- question_queue/views.py
+++ question_queue/views.py
@@ -17,7 +17,25 @@ def student(response):
 
 def instructor(response):
     template = loader.get_template("question_queue/instructor.html")
-    context = {"question_queue": ""}
+
+    test_data = [{
+        "id": "1",
+        "message": "Question One",
+    },
+    {
+        "id": "2",
+        "message": "Question Two",
+    },
+    {
+        "id": "3",
+        "message": "Question Three",
+    },
+    {
+        "id": "4",
+        "message": "Question Four",
+    }]
+
+    context = {"question_queue": "", "questions": test_data}
     return HttpResponse(template.render(context, response))
 
 
