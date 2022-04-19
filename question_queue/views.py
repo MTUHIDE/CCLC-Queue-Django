b1a87c24f36b56a4698eb0632042a3c543490d50
tjzetty <tjzetty@mtu.edu>
2022-04-18 22:58:20 -0400

    tr comps, added data to views, updated styles

--- question_queue/views.py
+++ question_queue/views.py
@@ -18,24 +18,36 @@ def student(response):
 def instructor(response):
     template = loader.get_template("question_queue/instructor.html")
 
-    test_data = [{
+    table_data = [{
         "id": "1",
+        "name": "Mark",
+        "class": "CS1121",
+        "time": "12:30",
         "message": "Question One",
     },
     {
         "id": "2",
+        "name": "Johnson",
+        "class": "CS1142",
+        "time": "12:55",
         "message": "Question Two",
     },
     {
         "id": "3",
+        "name": "Larry",
+        "class": "CS1121",
+        "time": "02:20",
         "message": "Question Three",
     },
     {
         "id": "4",
+        "name": "Susan",
+        "class": "CS1142",
+        "time": "02:55",
         "message": "Question Four",
     }]
 
-    context = {"question_queue": "", "questions": test_data}
+    context = {"question_queue": "", "questions": table_data}
     return HttpResponse(template.render(context, response))
 
 
