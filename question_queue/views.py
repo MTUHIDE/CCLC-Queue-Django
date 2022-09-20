f36b74068fa9f903544b8e3228e84843b3ecc4fa
kodew <kodewhitworth01@gmail.com>
2022-09-20 12:38:37 -0400

    Hooked up modals and table to database query

--- question_queue/views.py
+++ question_queue/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect
 
+from .models import Question
+
 from .forms import AnswerForm
 
 
@@ -27,36 +29,53 @@ def getAnswerForm(request):
 def instructor(request):
     user = "Dr.Professor"
 
-    table_data = [
-        {
-            "id": "1",
-            "name": "Mark",
-            "class": "CS1121",
-            "time": "12:30",
-            "message": "Question One",
-        },
-        {
-            "id": "2",
-            "name": "Johnson",
-            "class": "CS1142",
-            "time": "12:55",
-            "message": "Question Two",
-        },
-        {
-            "id": "3",
-            "name": "Larry",
-            "class": "CS1121",
-            "time": "02:20",
-            "message": "Question Three",
-        },
-        {
-            "id": "4",
-            "name": "Susan",
-            "class": "CS1142",
-            "time": "02:55",
-            "message": "Question Four",
-        },
-    ]
+    # table_data = [
+    #     {
+    #         "id": "1",
+    #         "name": "Mark",
+    #         "class": "CS1121",
+    #         "time": "12:30",
+    #         "message": "Question One",
+    #     },
+    #     {
+    #         "id": "2",
+    #         "name": "Johnson",
+    #         "class": "CS1142",
+    #         "time": "12:55",
+    #         "message": "Question Two",
+    #     },
+    #     {
+    #         "id": "3",
+    #         "name": "Larry",
+    #         "class": "CS1121",
+    #         "time": "02:20",
+    #         "message": "Question Three",
+    #     },
+    #     {
+    #         "id": "4",
+    #         "name": "Susan",
+    #         "class": "CS1142",
+    #         "time": "02:55",
+    #         "message": "Question Four",
+    #     },
+    # ]
+
+    # Query all questions
+    questions = Question.objects.all()
+
+    # Initialize data to be
+    table_data = []
+
+    # Add all query'd data the table that will passed as context
+    for question in questions:
+        question_info = {
+            "id": str(question.id),
+            "name": question.asked_by.first_name,
+            "class": question.course.name,
+            "time": question.created_at,
+            "message": question.message,
+        }
+        table_data.append(question_info)
 
     context = {
         "question_queue": "",
