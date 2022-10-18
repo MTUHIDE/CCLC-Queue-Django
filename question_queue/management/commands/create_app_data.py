6fdfa03f096ba38bc6cb25dd5a60712f55c0511d
kodew <kodewhitworth01@gmail.com>
2022-10-17 20:51:40 -0400

    Small Edit to Models and implemented rest of dummy data

--- question_queue/management/commands/create_app_data.py
+++ question_queue/management/commands/create_app_data.py
@@ -1,6 +1,6 @@
 from django.db import transaction
 from django.core.management.base import BaseCommand
-from question_queue.factories import UserFactory, CanvasCourseFactory
+from question_queue.factories import ReplyFactory, QueueQuestionFactory
 
 
 @transaction.atomic
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "Create's dummy data for the question queue app"
 
     def handle(self, *args, **kwargs):
-        print("Creating Users...\n")
+        print("Creating Dummy Data...\n")
         for _ in range(100):
-            UserFactory()
-            CanvasCourseFactory()
+            ReplyFactory()
+            QueueQuestionFactory()
--- question_queue/management/commands/delete_app_data.py
+++ question_queue/management/commands/delete_app_data.py
@@ -1,6 +1,13 @@
 from django.db import transaction
 from django.core.management.base import BaseCommand
-from question_queue.models import User, CanvasCourse
+from question_queue.models import (
+    Assignment,
+    Question,
+    User,
+    CanvasCourse,
+    Reply,
+    QueueQuestion,
+)
 
 
 @transaction.atomic
@@ -8,15 +15,30 @@ class Command(BaseCommand):
     help = "Deletes dummy data from our app"
 
     def handle(self, *args, **kwargs):
-        print("Deleting Users...\n")
+        print("Deleting Fake Data...\n")
 
         user_list = User.objects.all().exclude(is_superuser=True)
         course_list = CanvasCourse.objects.all()
+        assignment_list = Assignment.objects.all()
+        question_list = Question.objects.all()
+        reply_list = Reply.objects.all()
+        queue_question_list = QueueQuestion.objects.all()
+
+        # Deletes ALL dummy data in the app
+        for r in reply_list:
+            r.delete()
+
+        for q in question_list:
+            q.delete()
 
         for m in user_list:
-            print(m)
             m.delete()
 
         for c in course_list:
-            print(c)
             c.delete()
+
+        for a in assignment_list:
+            a.delete()
+
+        for z in queue_question_list:
+            z.delete()
