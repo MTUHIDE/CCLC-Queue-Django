6046d06588c70d251f7f88a34ed29e2a579fdedb
Jtkollod <jtkollod@mtu.edu>
2022-10-24 15:44:02 -0400

    Configured mysql

--- cclc_queue/settings.py
+++ cclc_queue/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "cclc_queue.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "cclc-queue",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
