eae3e1b9154e587ef734a39a80b3b329c418f5ef
tjquillan <tjquillan@gmail.com>
2022-11-08 13:28:56 -0500

    Migrate to Bootstrap 5
    This allowed for a more proper dark theme and also keeps the project on
    the latest Bootstrap.

--- cclc_queue/settings.py
+++ cclc_queue/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "question_queue",
     "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
